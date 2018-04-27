4bb86388a460905c0eb789393360cbe7be49b3b4
RafaelPedruzzi <rafaelbelmockpedruzzi@hotmail.com>
2018-04-27 13:54:07 +0100

    contribuicao.c

--- contribuicao/contribuicao.c
+++ contribuicao/contribuicao.c
@@ -20,39 +20,38 @@ struct tipoContribuicao
     int estado;
 };
 
+// Inicializa um struct contribuição
 Contribuicao* InicializaContribuicao(char* pagina, char* editor, char* arquivo)
 {
+    Contribuicao *c = (Contribuicao*) malloc(sizeof(Contribuicao)); // alocando espaço para a struct
 
-    Contribuicao *c = (Contribuicao*) malloc(sizeof(Contribuicao));
+    c->arquivo = (char*) malloc(strlen(arquivo) +1); // alocando espaço para string arquivo
+    strcpy(c->arquivo,arquivo);                      // copiando string arquivo
 
-    c->arquivo = (char*) malloc(strlen(arquivo) +1);
-    strcpy(c->arquivo,arquivo);
+    c->editor = (char*) malloc(strlen(editor) +1); // alocando espaço para string editor
+    strcpy(c->editor,editor);                      // copiando string editor
 
-    c->editor = (char*) malloc(strlen(editor) +1);
-    strcpy(c->editor,editor);
+    c->pagina = (char*) malloc(strlen(pagina) +1); // alocando espaço para string pagina
+    strcpy(c->pagina,pagina);                      // copiando string pagina
 
-    c->pagina = (char*) malloc(strlen(pagina) +1);
-    strcpy(c->pagina,pagina);
+    c->estado = 1; // indicando estado "presente"
 
-    c->estado = 1;
-
-    return c;
+    return c; // retornando contribuição
 }
 
-/*Retira uma contribuição de uma página da WIKED!
-* inputs: a contribuição
-* outputs: nenhum
-* pre-condição: contribuição válida
-* pos-condição: contribuição recebe estado "removido" (estado = 0)
-*/
-void RetiraContribuicao(Contribuicao* contribuicao);
+void RetiraContribuicao(Contribuicao* contribuicao)
+{
+    contribuicao->estado = 0;
+}
 
-void DestroiContribuicao(void* contribuicao){
-
+void DestroiContribuicao(void* contribuicao)
+{
+    // convertendo para Contrbuicao*
     Contribuicao *c = (Contribuicao*) contribuicao;
-
+    // Liberando as strings
     free(c->arquivo);
     free(c->editor);
     free(c->pagina);
+    // Liberando a contribuição
     free(c);
 }
